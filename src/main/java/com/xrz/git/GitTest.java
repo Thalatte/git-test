0e2d06712a9bfff07aee28ffc33dea99896c7317
Carpo <Carpo>
2022-08-14 10:28:19 +0800

    second commit

--- src/main/java/com/xrz/git/GitTest.java
+++ src/main/java/com/xrz/git/GitTest.java
@@ -3,5 +3,6 @@ package com.xrz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello  git!");
+        System.out.println("hello  git2!");
     }
 }
